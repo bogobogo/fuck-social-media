6c7ffbbd2c776aee7498d491e778f764e74e4bd3
Elad Bogomolny <bogomolnyelad@gmail.com>
2016-12-12 00:03:20 +0200

    first working version - only a handful of accounts right now

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import logging
 from google.appengine.api import mail
-from flask import Flask
-
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+import feedparser
+import datetime
+from time import mktime
 
 app = Flask(__name__)
 
@@ -11,21 +18,39 @@ def hello():
     return 'Hello World!'
 
 @app.route('/mail', methods=['GET'])
-def hello():
-	if request.method == 'POST':
-		ms = "דבר איתי כשאתה בבית".decode('utf-8')
-	                ## if it isnt send mail to the mail and 
-	    mail.send_mail(sender='Your daily tweets! <noreply@fuck-social-media.appspotmail.com>',
-	           to="bogo elad <bogomolnyelad@gmail.com>",
+def send_mail():
+	if request.method == 'GET':
+		following = ['paulg', 'NadavEyalDesk','sama', 'rabovitz', 'naval', 'nntaleb', 'tul', 'shapiro', 'cpojer', 'chamath', 'patrickc', 'collision']
+		emaildata = []
+		for name in following:
+			page = feedparser.parse('http://twitrss.me/twitter_user_to_rss/?user=' + name)
+			username = page['feed']['title'].split()[-1]
+			for i in page.entries:
+				emaildata.append({ 'username' : username, 'tweet' : i.title, 'date': i.published_parsed})
+
+			
+		emaildata.sort(key=lambda item:item['date'], reverse=True)
+		logging.info(emaildata)
+		yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+		logging.info(yesterday)
+		dataFiltered = [i for i in emaildata if datetime.datetime.fromtimestamp(mktime(i['date'])) > yesterday]
+		logging.info(dataFiltered)
+
+		logging.info('1')
+        ms = render_template('tweets_email.html', emaildata = dataFiltered)
+        mail.send_mail(sender='Your daily tweets! <noreply@fuck-social-media.appspotmail.com>',
+               to="bogo elad <bogomolnyelad@gmail.com>",
 	           subject="You were invited to join a notebook!",
 	           body="""Dear NIV:
+               works just need to add hebrew support
+               I am free to talk now
+               daw
+               The legati Team
+               """,
+              html="<head> <meta charset='utf-8' /> </head>" + ms ) 
+        
+        return 'Hello World!'
 
-	            works just need to add hebrew support
-	            I am free to talk now
-	            daw
-	            The legati Team
-	            """,
-	            html="<head> <meta charset='utf-8' /> </head><h3>" + ms + " </h3>") 
 
 
 @app.errorhandler(500)
